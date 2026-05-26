6aa9e915ec667eaea559165c210b52df2465e7be
rvfrontend <rvottens@gmail.com>
2026-05-26 23:08:58 +0200

    Fix: Change default consent mode to granted for GA detection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,8 +124,8 @@ export default async function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('consent', 'default', {
-              'analytics_storage': 'denied',
-              'ad_storage': 'denied',
+              'analytics_storage': 'granted',
+              'ad_storage': 'granted',
               'ad_user_data': 'denied',
               'ad_personalization': 'denied'
             });
